e39343b3f05420fe12222beeb05a3ea9e8626b46
JL Cox <jl.cox@bemaservices.com>
2021-07-15 08:15:27 -0500

    Changes after redirection

--- API/client.py
+++ API/client.py
@@ -1,10 +1,10 @@
 import requests
 from config import Config
-from json import loads
 
-
+# TEST after Redirection
 class Client:
 
+    # Establishes key components
     def __init__(self, api_token=Config.api_key, limit=10, count_only=False) -> None:
         self.api_token = api_token
         self.base_url = "https://usea1-swprd1.sentinelone.net/web/api/v2.1"
@@ -13,25 +13,22 @@ class Client:
         self.limit = limit
         self.count_only = count_only
 
-
+    # Sends a "Get Agents" request and confirms a 200 status code is returned
     def api_test(self):
         full_url = self.base_url + self.options["agents"]
+
         response = requests.get(full_url, headers=self.auth_header)
         if response.status_code == 200:
             return "Connection to the API was successfull"
         else:
             return f"Connection to the API has failed with code: {response.status_code}"
-            
-
-    def create_account(self):
-        pass
         
-
+    # Get the Accounts, and their data, that match the filter.
     def get_accounts(self):
         full_url = self.base_url + "/accounts"
         return requests.get(full_url, headers=self.auth_header)
 
-
+    # Get Account data from a given Account ID. 
     def get_account_by_id(self, account_id):
         if int(account_id):
             full_url = self.base_url + f"accounts/{account_id}"
@@ -39,24 +36,25 @@ class Client:
         else:
             TypeError(f"account_id must be of type int and not '{type(account_id)}'")
 
-
+    # Get the activities, and their data, that match the filters. 
     def get_activities(self):
         full_url = self.base_url + "/activities"
         return requests.get(full_url, headers=self.auth_header)
 
-
+    # Get a list of activity types 
     def get_activity_types(self):
         full_url = self.base_url + "/activities/types"
         return requests.get(full_url, headers=self.auth_header) 
 
 
+    # Get the Syslog message that corresponds to the last activity that matches the filter.
     def get_last_syslog_activity(self):
         full_url = self.base_url + "/last-activity-as-syslog"
         return requests.get(full_url, headers=self.auth_header)
 
-
+    
+    # Get the uninstall password to uninstall several Agents of one Account with one command.
     def get_uninstall_password(self, account_id):
-
         if int(account_id):
             full_url = self.base_url + f"/accounts/{account_id}/uninstall-password/view"
             return requests.get(full_url, headers=self.auth_header)
@@ -64,22 +62,10 @@ class Client:
             TypeError(f"account_id must be of type int and not '{type(account_id)}'")
 
 
+    # Get the uninstall password metadata, such as which user created and revoked it and when.
     def get_uninstall_password_metadata(self, account_id):
-
         if int(account_id):
             full_url = self.base_url + f"/accounts/{account_id}/uninstall-password/metadata"
             return requests.get(full_url, headers=self.auth_header)
         else:
             TypeError(f"account_id must be of type int and not '{type(account_id)}'")
-
-
-    def decommission(self, id_list):
-
-        full_url = self.base_url + "/agents/actions/decommission"
-
-        if type(id_list) == list or len(id_list) == 0:
-            command = loads(str({"data":{}, "filter":  {"ids": id_list}}).replace("'", "\""))
-            return requests.post(full_url, headers=self.auth_header, json=command)
-        else:
-            TypeError(f"Arg 'id_list' must be of type list and not be empty'")
-
